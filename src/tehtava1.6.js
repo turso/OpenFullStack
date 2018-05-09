e59e165f06338f3b389b0493092fd103d16154e7
Oliver Rikkilä <oliver.rikkila@siilim160906.local>
2018-05-09 22:45:02 +0300

    tehtava 1.7

--- src/tehtava1.6.js
+++ src/tehtava1.6.js
@@ -9,8 +9,7 @@ class App extends Component {
       statistic: 'statistiikka',
       hyva: 0,
       neutraali: 0,
-      huono: 0,
-      totalVotes: 0
+      huono: 0
     };
     this.addHyva = this.addHyva.bind(this);
     this.addNeutraali = this.addNeutraali.bind(this);
@@ -19,34 +18,23 @@ class App extends Component {
 
   addHyva = () => {
     this.setState(prevState => ({
-      hyva: prevState.hyva + 1,
-      totalVotes: prevState.totalVotes + 1
+      hyva: prevState.hyva + 1
     }));
   };
 
   addNeutraali = () => {
     this.setState(prevState => ({
-      neutraali: prevState.neutraali + 1,
-      totalVotes: prevState.totalVotes + 1
+      neutraali: prevState.neutraali + 1
     }));
   };
 
   addHuono = () => {
     this.setState(prevState => ({
-      huono: prevState.huono + 1,
-      totalVotes: prevState.totalVotes + 1
+      huono: prevState.huono + 1
     }));
   };
 
   render() {
-    const prosentti = () => {
-      return (this.state.hyva / this.state.totalVotes * 100).toFixed(1);
-    };
-
-    const keskiarvo = () => {
-      return ((this.state.hyva - this.state.huono) / this.state.totalVotes).toFixed(1);
-    };
-
     return (
       <div>
         <h1>{this.state.topic}</h1>
@@ -60,11 +48,7 @@ class App extends Component {
         <div>Hyvä {this.state.hyva}</div>
         <div>Neutraali {this.state.neutraali}</div>
         <div>Huono {this.state.huono}</div>
-        <div>Keskiarvo {keskiarvo()}</div>
-        <div>Positiviisia {prosentti()}% </div>
       </div>
     );
   }
 }
-
-ReactDOM.render(<App />, document.getElementById('root'));
