5d712ff15ad42ceadcf3aa9a1cfbd77c4cdfc7eb
Oliver Rikkilä <oliver.rikkila@siili.com>
2018-05-16 21:43:06 +0300

    tehtava 2.13 kesken

--- src/components/CountryList.js
+++ src/components/CountryList.js
@@ -8,6 +8,26 @@ const CountryList = ({ data, filter }) => {
           return country.name.indexOf(filter) !== -1;
         });
 
+  const showDetails = country => {
+    console.log('KLIKATTU!!');
+    console.log('country', country);
+    console.log('nimi', country.name);
+    return (
+      <div>
+        <div>
+          <h1>
+            {country.name} {country.nativeName}
+          </h1>
+          <p>capital: {country.capital}</p>
+          <p>population: {country.population}</p>
+          <p>
+            <img alt="flag" src={country.flag} />
+          </p>
+        </div>
+      </div>
+    );
+  };
+
   if (countryToShow.length === 1) {
     return (
       <div>
@@ -28,7 +48,15 @@ const CountryList = ({ data, filter }) => {
   } else if (countryToShow.length > 10) {
     return <div>too many matches, specify another filter</div>;
   } else {
-    return <div>{countryToShow.map(country => <div key={country.alpha2Code}>{country.name}</div>)}</div>;
+    return (
+      <div>
+        {countryToShow.map(country => (
+          <div key={country.alpha2Code} onClick={() => showDetails(country)}>
+            {country.name}
+          </div>
+        ))}
+      </div>
+    );
   }
 };
 
