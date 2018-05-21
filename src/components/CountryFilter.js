3aab49cdb6cbf19cc8f48625502f1dc4d0944703
Oliver Rikkilä <oliver.rikkila@siili.com>
2018-05-21 12:52:17 +0300

    Siirtelyä osa 2

--- src/components/CountryFilter.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-const CountryFilter = ({ filter, input }) => {
-  console.log('filter', filter);
-  return <input value={filter} onChange={input} />;
-};
-
-export default CountryFilter;
